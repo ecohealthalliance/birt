72533dba6be9c5d4b686dea5110c2e62a11c5ce8
dan-nyanko <dnyanko@dtec.com>
2016-03-31 12:51:44 -0400

    removed nouislider dependency from app and added to scrubber/packages
    added momentjs
    added moment-range

--- app/packages/scrubber/package.js
+++ app/packages/scrubber/package.js
@@ -9,14 +9,17 @@ Package.onUse(function(api){
     'templating',
     'coffeescript',
     'mquandalle:stylus@1.1.1',
+    'momentjs:moment@2.10.6',
+    'kevbuk:moment-range@2.2.2',
+    'crystalhelix:nouislider@1.0.1',
     'reactive-var'
   ], 'client');
 
   api.addFiles([
     'stylesheets/scrubber.styl',
-
     'templates/scrubber.html',
-
     'scrubber.coffee'
   ], 'client');
+
+
 });
